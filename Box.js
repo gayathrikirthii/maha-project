905847b8b729342516c063ce8bcb0004a8abb3dd
gayathrikirthii <70528712+gayathrikirthii@users.noreply.github.com>
2020-10-24 12:04:51 +0100

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,19 @@
+class Box extends BaseClass{
+
+    constructor(x, y, width, height){
+
+super(x,y,width,height);
+
+this.image = loadImage("sprites/wood1.png")
+//Matter.Body.setAngle(this.body.angle);
+
+}
+
+
+
+
+
+
+
+
+}
